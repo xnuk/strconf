e42e2e1c16d0c7732cbfb2737f9ae3922d8a5609
Xnuk Shuman <admin@xnu.kr>
2024-04-10 15:44:09 +0900

    some useful example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{stdout, Write};
+use std::path::Path;
 use std::{env, fs};
 
 use anyhow::anyhow;
@@ -9,9 +10,27 @@ fn main() -> anyhow::Result<()> {
 		return Err(anyhow!("No file path given"));
 	};
 
-	let file = fs::read_to_string(input_path)?;
-	let parsed: Config = toml::from_str(&file)?;
-	let unparsed = toml::to_string_pretty(&parsed)?;
+	let input_path: &Path = input_path.as_ref();
+
+	let ext = input_path
+		.extension()
+		.and_then(|x| x.to_str())
+		.map(|x| x.to_ascii_lowercase().into_bytes())
+		.unwrap_or_default();
+
+	let file = || fs::read_to_string(input_path);
+
+	let parsed: Config = if ext == b"json" {
+		serde_json::from_str(&file()?)?
+	} else if ext == b"yaml" || ext == b"yml" {
+		serde_yaml::from_str(&file()?)?
+	} else if ext == b"toml" {
+		toml::from_str(&file()?)?
+	} else {
+		return Err(anyhow!("File name should end with supported format"));
+	};
+
+	let unparsed = serde_json::to_string_pretty(&parsed)?;
 	stdout().write_all(unparsed.as_bytes())?;
 
 	Ok(())
